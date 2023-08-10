cabda4ad464f4f60cc0cf00eb83d627e6eb12b43
Md.Amir Khan <mdkhan@gedu.global>
2023-08-10 17:30:19 +0530

    implement student page

--- src/app/students/page.jsx
+++ src/app/students/page.jsx
@@ -1,14 +1,88 @@
+"use client"
 import { Container } from "@mui/material";
-import React from "react";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CreateIcon from '@mui/icons-material/Create';
+import Link from "next/link";
 
-const page = () => {
+
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+export default function student() {
+  const [data, setData] = useState([]);
+  const [record, setRecord] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+   .then(res=>{
+    setData(res.data);
+    setRecord(res.data);
+   })
+   .catch(error => console.log(error))
+  }, []);
   return (
-    <>
-      <Container className="modify_container">
-        <h1>Students content</h1>
-      </Container>
-    </>
+    <Container className="modify_container">
+         <TableContainer component={Paper} >
+      <Table  aria-label="customized table" >
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell align="right">Name</StyledTableCell>
+            <StyledTableCell align="right">User Name</StyledTableCell>
+            <StyledTableCell align="right">Email(g)</StyledTableCell>
+            <StyledTableCell align="right">Action(g)</StyledTableCell>
+          </TableRow>
+          
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.id}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.name}</StyledTableCell>
+              <StyledTableCell align="right">{row.username}</StyledTableCell>
+              <StyledTableCell align="right">{row.email}</StyledTableCell>
+              <StyledTableCell align="right">
+              <Link href={`/students/${row.id}`}>
+                <CreateIcon/>
+                </Link>
+                </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Container>
   );
-};
+}
 
-export default page;
